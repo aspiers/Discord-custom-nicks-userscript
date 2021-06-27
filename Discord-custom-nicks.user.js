e9baa15264d56ec4721e20d1d8285049ada0ebf0
Adam Spiers <userscripts@adamspiers.org>
2021-06-27 22:02:16 +0100

    Rename file to have .user.js suffix
    This seems to be the convention and may help with automatic installation.

--- Discord-custom-nicks.user.js
+++ Discord-custom-nicks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord custom nicknames
 // @namespace    https://github.com/aspiers/Discord-custom-nicks-userscript
-// @version      0.1.2
+// @version      0.1.3
 // @description  Assign custom names to Discord nicknames client-side
 // @author       Adam Spiers
 // @match        https://discord.com/channels/*
@@ -9,7 +9,7 @@
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=115012
-// @downloadURL  https://raw.githubusercontent.com/aspiers/Discord-custom-nicks-userscript/main/Discord-custom-nicks-userscript.js
+// @downloadURL  https://raw.githubusercontent.com/aspiers/Discord-custom-nicks-userscript/main/Discord-custom-nicks.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
